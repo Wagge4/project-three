25c1cec97b79b9ff4e689c17c2995a07e546a0a4
Wagge4 <markus.wallgren4@gmail.com>
2022-06-08 22:25:27 +0000

    Add else statement to the guess part and fix comments

--- run.py
+++ run.py
@@ -1,16 +1,16 @@
 # Imports the random module to make the game work.
 import random
 
-number = input("Kindly type a number higher than 1, for the upper range: ")
+number = input("Kindly type a number for the upper range, number has to be higher than one: ")
 
 # If statement that checks if the value that was put in is a digit
 if number.isdigit():
     number = int(number)
 
-# Else statement that will print out if the user inputs something other than a number < 1
+# Else statement that will print out if the user inputs something other than a number higher than 1
 else:
     print("You did not insert a number higher than 1, kindly do this next time. Game over.")
-    quit() 
+    quit()
 
 guess_number = random.randint(1, number)
 attempts = 0
@@ -18,6 +18,10 @@ attempts = 0
 # While loop for the guessing part of the game
 while True:
     attempts += 1
-    guess_user = input("Make your guess of the random number here: ")
+    guess_user = input(f"Make your guess of the random number(between 1-{number}) here: ")
     if guess_user.isdigit():
         guess_user = int(guess_user)
+# Else statement in case the user makes an error with the input.
+    else:
+        print("You have to guess a number, try again!")
+        continue
