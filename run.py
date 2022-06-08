a8585dc47e2205d808d3e6da5a730444117638f2
Wagge4 <markus.wallgren4@gmail.com>
2022-06-08 20:57:54 +0000

    Add else statement and random.randint, also fix bug with previous code

--- run.py
+++ run.py
@@ -1,7 +1,15 @@
 # Imports the random module to make the game work.
 import random
 
-number = input("Kindly type a number for the top range: ")
+number = input("Kindly type a number higher than 1, for the upper range: ")
 
+# If statement that checks if the value that was put in is a digit
 if number.isdigit():
-    print("Let's play!")
+    number = int(number)
+
+else:
+    print("You did not insert a number higher than 1, kindly do this next time. Game over.")
+    quit() 
+
+guess_number = random.randint(1, number)
+attempts = 0
