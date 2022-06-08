89d18227e5fb29c48e2c0e88cd36c4254d556957
Wagge4 <markus.wallgren4@gmail.com>
2022-06-08 21:35:03 +0000

    Add while loop for the guess part

--- run.py
+++ run.py
@@ -7,9 +7,17 @@ number = input("Kindly type a number higher than 1, for the upper range: ")
 if number.isdigit():
     number = int(number)
 
+# Else statement that will print out if the user inputs something other than a number < 1
 else:
     print("You did not insert a number higher than 1, kindly do this next time. Game over.")
     quit() 
 
 guess_number = random.randint(1, number)
 attempts = 0
+
+# While loop for the guessing part of the game
+while True:
+    attempts += 1
+    guess_user = input("Make your guess of the random number here: ")
+    if guess_user.isdigit():
+        guess_user = int(guess_user)
