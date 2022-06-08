fdf2ab9a55c282497bf435e18d9ba270a6ce8c02
Wagge4 <markus.wallgren4@gmail.com>
2022-06-08 22:47:53 +0000

    Add code to the last part of the game where user wins

--- run.py
+++ run.py
@@ -7,21 +7,33 @@ number = input("Kindly type a number for the upper range, number has to be highe
 if number.isdigit():
     number = int(number)
 
-# Else statement that will print out if the user inputs something other than a number higher than 1
+# Else statement that will print out if the user inputs something other than a number higher than one.
 else:
-    print("You did not insert a number higher than 1, kindly do this next time. Game over.")
+    print("You did not insert a number higher than one, kindly do this next time. Game over.")
     quit()
 
 guess_number = random.randint(1, number)
 attempts = 0
 
-# While loop for the guessing part of the game
+# While loop for the guessing part of the game.
 while True:
     attempts += 1
     guess_user = input(f"Make your guess of the random number(between 1-{number}) here: ")
     if guess_user.isdigit():
         guess_user = int(guess_user)
+
 # Else statement in case the user makes an error with the input.
     else:
         print("You have to guess a number, try again!")
+        # This makes the loop continue so that the game wouldn't have to restart.
         continue
+
+# If statement for when the user gets the correct number.
+    if guess_user == guess_number:
+        print("Great work, you guessed the right number!!")
+        # This breaks the loop as the user has already won the game.
+        break
+    elif guess_user < guess_number:
+        print("Your guess is too low. Try again and go a little higher!\n")
+    else:
+        print("Your guess is too high. Try again and go a little lower!\n")
