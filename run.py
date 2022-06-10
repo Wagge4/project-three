a73dd2b08c4e11107834e83a6fad5fe2b442a7a4
Wagge4 <markus.wallgren4@gmail.com>
2022-06-10 15:52:45 +0000

    Add new line on inputs to make code ready for deployment

--- run.py
+++ run.py
@@ -1,13 +1,14 @@
 # Imports the random module to make the game work.
 import random
 
-number = input("Kindly type a number for the upper range, number has to be higher than one: ")
+number = input("Kindly type a number for the upper range, number has to be higher than one:\n ")
 
 # If statement that checks if the value that was put in is a digit
 if number.isdigit():
     number = int(number)
 
-# Else statement that will print out if the user inputs something other than a number higher than one.
+# Else statement that will print out if the user inputs
+# something other than a number higher than one.
 else:
     print("You did not insert a number higher than one, kindly do this next time. Game over.")
     quit()
@@ -18,14 +19,15 @@ attempts = 0
 # While loop for the guessing part of the game.
 while True:
     attempts += 1
-    guess_user = input(f"Make your guess of the random number(between 1-{number}) here: ")
+    guess_user = input(f"Make your guess of the random number(between 1-{number}) here:\n ")
     if guess_user.isdigit():
         guess_user = int(guess_user)
 
 # Else statement in case the user makes an error with the input.
     else:
         print("You have to guess a number, try again!")
-        # This makes the loop continue so that the game wouldn't have to restart.
+        # This makes the loop continue so that
+        # the game wouldn't have to restart.
         continue
 
 # If statement for when the user gets the correct number.
@@ -38,8 +40,9 @@ while True:
     else:
         print("\nYour guess is too high. Try again and go a little lower!")
 
-# This prevents the issue of printing out "Completed in 1 guessES", to make the english correct,
+# This prevents the issue of printing out "Completed in 1 attemptS"
+# to make the english correct,
 if attempts == 1:
     print("It took you 1 attempt to guess the right number, great job!")
 else:
-    print("It took you", attempts, "attempts to guess the right number, great job!")
+    print("It took you", attempts, "attempts to guess the right number!")
